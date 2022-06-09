d5f6aab1f45055eeacbf81e6285e2b0511d461f3
Kim <howtoad123@gmail.com>
2022-06-09 14:06:05 +0200

    still basics set ups

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { Link } from "react-router-dom";
+import { css } from "@emotion/react";
+const Navbar = () => {
+  // const styles = {
+  //     example: `
+  //     `
+  // }
+  return (
+    <nav>
+      <Link to="/">Newsbox</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // console.log(useLocation().pathname));
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* <Route path="archive" element={<Archive />} />
+        <Route path="settings" element={<Settings />} /> */}
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>
 );
